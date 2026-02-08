4afcda3eb12045f7117c7c720d053e64bc9d248b
DarkKnight8247 <franz.castor25@gmail.com>
2026-02-08 10:10:32 +0800

    Add files via upload

---
+++ AMbot/SampleModel.cs
@@ -0,0 +1,11 @@
+namespace k.Models
+{
+    public class SampleModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Course { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+    }
+}
